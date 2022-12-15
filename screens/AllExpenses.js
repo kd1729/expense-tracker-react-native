e5f5d45a7629a7bf1607b7bd637bfb04cea81170
onlykingKD <kaustubhdwivedi1729@gamil.com>
2022-12-15 19:30:55 +0530

    Initialized Screens

---
+++ screens/AllExpenses.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default function AllExpenses(){
+    return(
+        <View>
+            <Text>All Expenses</Text>
+        </View>
+    )
+}
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default function RecentExpenses(){
+    return(
+        <View>
+            <Text>Recent Expenses</Text>
+        </View>
+    )
+}
